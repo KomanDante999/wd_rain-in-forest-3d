f1b1bbd234004b86077ea9627d34cb3c39d50fca
komandante999 <dreftware@yandex.ru>
2023-12-24 13:21:08 +0700

    add free rotate

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
+let indexRotate = .01
+let freeRotate = false
+
 document.addEventListener('mousemove', ev => {
 	Object.assign(document.documentElement, {
 		style: `
-			--moveX: ${(ev.clientX - window.innerWidth / 2) * -.005}deg;
-			--moveY: ${(ev.clientY - window.innerHeight / 2) * -.01}deg;
+			--moveX: ${(ev.clientX - window.innerWidth / 2) * (indexRotate * -.5)}deg;
+			--moveY: ${(ev.clientY - window.innerHeight / 2) * (indexRotate * -1)}deg;
 		`
 	})
 })
+
+document.getElementById('background').addEventListener('mousedown', event => {
+	if (!event._clickOnButton) {
+		indexRotate = .01;
+		freeRotate = false;
+	}
+})
+
+document.getElementById('button-start').addEventListener('mousedown', event => {
+	console.log('button-start');
+	event._clickOnButton = true
+	if (!freeRotate) {
+		freeRotate = true;
+		indexRotate = .5;
+	}
+})
+
+
